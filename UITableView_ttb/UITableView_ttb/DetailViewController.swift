49340764439956487d81e411abb5f6b49a44e270
thanhbinh13131 <thanhbinh13131@gmail.com>
2017-04-22 16:48:57 +0700

    Add files via upload

---
+++ UITableView_ttb/UITableView_ttb/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  UITableView_ttb
+//
+//  Created by admin25 on 4/22/17.
+//  Copyright © 2017 admin25. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    @IBOutlet weak var eventTitle: UILabel!
+    
+    @IBOutlet weak var eventContent: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
